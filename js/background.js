b21531161bc8db59a4064b1391814f4cff37b928
vdorchan <vdorchan@gmail.com>
2020-07-24 13:40:04 +0800

    feat：update

--- js/background.js
+++ js/background.js
@@ -8,8 +8,7 @@ let taobaoCookiesStr = ''
 
 console.log('load background')
 
-const url =
-  'https://h5api.m.taobao.com/h5/mtop.alibaba.iic.xinsightshop.olap.query/1.0/?jsv=2.5.8&appKey=27522521&t=1594347265190&sign=d48a35e48b0e6d1ff2dad12890e0ce66&api=mtop.alibaba.iic.xinsightshop.olap.query&v=1.0&type=originaljson&dataType=json&timeout=20000&H5Request=true&data=%7B%22param%22%3A%22%7B%5C%22queryId%5C%22%3A%5C%2211%7C49336521%7Cundefined%5C%22%2C%5C%22cubeId%5C%22%3A%5C%22tblive_rpt_item_indicator%5C%22%2C%5C%22queryDetail%5C%22%3Afalse%2C%5C%22startTime%5C%22%3A%5C%222020-07-07%2020%3A01%3A09%5C%22%2C%5C%22endTime%5C%22%3A%5C%222020-07-07%2022%3A02%3A59%5C%22%2C%5C%22timeType%5C%22%3A2%2C%5C%22sign%5C%22%3Anull%2C%5C%22limit%5C%22%3A2000%2C%5C%22row%5C%22%3A%5C%22%5B%7B%5C%5C%5C%22name%5C%5C%5C%22%3A%5C%5C%5C%22%E5%95%86%E5%93%81id%5C%5C%5C%22%2C%5C%5C%5C%22isMeasure%5C%5C%5C%22%3Afalse%7D%2C%7B%5C%5C%5C%22name%5C%5C%5C%22%3A%5C%5C%5C%22%E4%B8%BB%E5%9B%BE%E5%9C%B0%E5%9D%80%5C%5C%5C%22%2C%5C%5C%5C%22isMeasure%5C%5C%5C%22%3Afalse%7D%2C%7B%5C%5C%5C%22name%5C%5C%5C%22%3A%5C%5C%5C%22%E5%95%86%E5%93%81%E6%A0%87%E9%A2%98%5C%5C%5C%22%2C%5C%5C%5C%22isMeasure%5C%5C%5C%22%3Afalse%7D%5D%5C%22%2C%5C%22measure%5C%22%3A%5C%22%5B%7B%5C%5C%5C%22name%5C%5C%5C%22%3A%5C%5C%5C%22%E5%95%86%E5%93%81%E7%82%B9%E5%87%BB%E6%AC%A1%E6%95%B0%5C%5C%5C%22%2C%5C%5C%5C%22isMeasure%5C%5C%5C%22%3Atrue%7D%2C%7B%5C%5C%5C%22name%5C%5C%5C%22%3A%5C%5C%5C%22%E5%95%86%E5%93%81%E7%82%B9%E5%87%BB%E4%BA%BA%E6%95%B0%5C%5C%5C%22%2C%5C%5C%5C%22isMeasure%5C%5C%5C%22%3Atrue%7D%5D%5C%22%2C%5C%22column%5C%22%3A%5C%22%5B%5D%5C%22%2C%5C%22orders%5C%22%3A%5C%22%5B%5D%5C%22%2C%5C%22filter%5C%22%3A%5C%22%5B%7B%5C%5C%5C%22name%5C%5C%5C%22%3A%5C%5C%5C%22%E5%BC%80%E6%92%AD%E6%97%A5%E6%9C%9F%E5%88%86%E5%8C%BA%E5%AD%97%E6%AE%B5yyyymmdd%5C%5C%5C%22%2C%5C%5C%5C%22type%5C%5C%5C%22%3A%5C%5C%5C%22dimension%5C%5C%5C%22%2C%5C%5C%5C%22isMeasure%5C%5C%5C%22%3Afalse%2C%5C%5C%5C%22values%5C%5C%5C%22%3A%5B%5C%5C%5C%2220200707%5C%5C%5C%22%5D%2C%5C%5C%5C%22oper%5C%5C%5C%22%3A%5C%5C%5C%22%3D%5C%5C%5C%22%7D%2C%7B%5C%5C%5C%22name%5C%5C%5C%22%3A%5C%5C%5C%22%E7%9B%B4%E6%92%AD%E9%97%B4id%5C%5C%5C%22%2C%5C%5C%5C%22type%5C%5C%5C%22%3A%5C%5C%5C%22dimension%5C%5C%5C%22%2C%5C%5C%5C%22isMeasure%5C%5C%5C%22%3Afalse%2C%5C%5C%5C%22values%5C%5C%5C%22%3A%5B%5C%5C%5C%22271421666877%5C%5C%5C%22%5D%2C%5C%5C%5C%22oper%5C%5C%5C%22%3A%5C%5C%5C%22%3D%5C%5C%5C%22%7D%2C%7B%5C%5C%5C%22name%5C%5C%5C%22%3A%5C%5C%5C%22%E4%B8%BB%E6%92%ADid%5C%5C%5C%22%2C%5C%5C%5C%22type%5C%5C%5C%22%3A%5C%5C%5C%22dimension%5C%5C%5C%22%2C%5C%5C%5C%22isMeasure%5C%5C%5C%22%3Afalse%2C%5C%5C%5C%22values%5C%5C%5C%22%3A%5B2081592196%5D%2C%5C%5C%5C%22oper%5C%5C%5C%22%3A%5C%5C%5C%22%3D%5C%5C%5C%22%7D%5D%5C%22%2C%5C%22extra%5C%22%3Anull%7D%22%2C%22innerId%22%3A%22%22%7D'
+// const url = 'https://h5api.m.taobao.com/h5/mtop.alibaba.iic.xinsightshop.olap.query/1.0/?jsv=2.5.8'
 
 function setHeader(headers, header) {
   let hasSet = false
@@ -25,17 +24,74 @@ function setHeader(headers, header) {
   }
 }
 
+async function fetchTaobao(url) {
+  let _data = await fetch(url, {
+    referrerPolicy: 'no-referrer-when-downgrade',
+    body: null,
+    method: 'GET',
+    mode: 'cors',
+    credentials: 'include',
+  })
+  _data = await _data.json()
+
+  return _data
+}
+
+// async function fetchTaobao(url) {
+//   return new Promise((resolve, reject) => (
+//     $.get(url, data => resolve(data))
+//   ))
+// }
+
+async function queryUrl(url) {
+  return new Promise((resolve, reject) => $.get(url, (data) => resolve(data)))
+}
+
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function (details) {
+    console.log({ details })
+    if (details.tabId === -1) {
+      if (details.url.includes('h5api.m.taobao.com')) {
+        // console.log('header', details.requestHeaders);
+        setHeader(details.requestHeaders, {
+          name: 'Referer',
+          value: LIVE_PAGE_URL,
+        })
+        setHeader(details.requestHeaders, {
+          name: 'Origin',
+          value: 'https://databot.taobao.com',
+        })
+        setHeader(details.requestHeaders, {
+          name: 'origin',
+          value: 'https://databot.taobao.com',
+        })
+        setHeader(details.requestHeaders, {
+          name: 'Sec-Fetch-Site',
+          value: 'same-site',
+        })
+        setHeader(details.requestHeaders, {
+          name: 'accept',
+          value: 'application/json',
+        })
+        setHeader(details.requestHeaders, {
+          name: 'bx-umidtoken',
+          value: 'T2gAZawjIGWojnNFN0-a2HZNDNxvtSrMT_N3bnNuX5Tig2ELVHM_pg8X77s1DspSqH6Vg9zAODVXzxEeygV54W5S'
+        })
+        setHeader(details.requestHeaders, {
+          name: 'bx-ua',
+          value: '130#dwoHrDFFwZyDODxruDHDkCC4xQYDDD997EOBfQRVDx7EwFbiDfSW4XafVrHWTQx9/sfyDP0Dco+vpWDOFIiQmnjXH2PfDD99kkU+HUOyQOQGrwDDSt0DQWgySD9I62tcSnBDSI9KDxYFq+kliEv5TlSDD+mN2hBjMDDYSsj0SHE0FDOlmGJ5U0SxHbYAsF4v9N4xr3gJtXQG6LxNDzm+887XUky6ukqAFIfXTivU9KUm6CfRdGgA0iDx563/rO4C75VAIwgf7sECgM04+lJ98QMz7/aGsOlXFL9AxEy4ITrkc50nqlK2wE7mw7me593YPH+JY0m/xrmIDSRLVGH788FWlUH6skxgO2rxd3VU+JIx5G0RO/0ChQ2zFiXauhWsDllzjiV/kMsxQtI4xLkIGkDApwgdui+1WtriFcZhIgZPog0nrlipch2ONCYNp9x9tJ+krNmJ53r4FYOlph40TnSYD+MN2Ea/MDQDMlmX23r0FDOlphJ5TnSYD+Ml29LcMDPpoRJ52lrNzWFH6vdMDlSDDFPNooHMYDQDaDY8rYvHZEz+C0RQgj6sCf1n8hShv04ab2mJQflpRcd6X8s6K2cFdbnLWtb9pZaSlOfxtzwDCLXVhA1C0r9kQ09gyJ6STWRpMyTG4BDZzsewFTZICWahb7cRGe9eT2ATA1AWj153B6aJpmjG44qeY2x4gdlt25mKAxIwU/uBBrILz9OaNNecbzq8+G/mclEXez0EcsDHhr59betA025QGc0GyPIuM7OrhSH/eR91ebPnNgXVqjzlxv+Atrmlg/DF69IKvmRS526bAEIofhHvfOShKHtECZoMq6AprDRQVx1TPCPeuh1GhI3xwUnX9CcAtbpBOL7xYrjWfYU/J9mvMfA02hcyBcLzgViogROap7KB4aBjSRNGS++HU03Z2K9Bqyud7zwaDqgNReCp/9Lyx/+6p4kKWML1Xg+a5hdo'
+        })
+        // setHeader(details.requestHeaders, { name: 'bx-ua', value: '125#coPcaW1PcWQTTwQKzNfjQPdSuDf2r14YEXzdh0wkDabeTy+7lLrzizUry2DYvGZJP5aGwjm+HikyYKtwz6J8+ArgPTl5We9NsJtJOiNbljnNvcYsdbW3FLgx2z4tReU3AphNThGsBlOW8t41Mg93Gtr2x5CZy0NQ4acGcUiccgapEgscUXoScvsONhMpc37fJmwP+lKBN+XWapS1SaILV5jdQMmfDZ4AoH7Cllu0VhVulFCP700YSoDhaeU9U89QNLpLDIlnpiMTFtkcSUSOlRqdMcTlw/U/5zenxL8huRUd46mrV0B+oYU66G5mryG5SD4ReFEyPxG9k/HeVABMcKUceFW94TFC+QoZt+4g2e4f2Waf9dV8TH1IEbNOdHPhmFjwPq2MWp0L2ANuaQsZob7Cta31lVeUL34XDx5KFcfml8gt2tL4eIGLJLvb2oW/PikngUF8O/SqLy8Lx7nBI+/lLHIREvY92wh8HGWGhzD/qUMrRbo/poN0zUukj+KixfG+Xo2UV6gOXv5m9tWxeh8g9utz2kOjnZ7D3xpuUrWKrRgOKw1IEzuN095JGkcSRKmAbJh/iT4r4vkXGA/7g66ZLseQz/KphGAr3CQtFBGGVUrx5JiCasvK6XmLGfCOkMl1hdg2UHbY/FSKZ9+PWwpD32qgx5VvrrcWW5CszfQMVdcxFiETZ0E7fa5zs7swj8of7dnKj37GHoRiNRX8w3yTisjU5JphMr2xTzoRY66YCXfGHaAdlev3L11zLoeERm2slIAko8o8936X9ZZPzw3v+0OZzzxaoze7uGpJIcQQCfZXsbdvZwTekRd6VX7X1OVZ/dVGn1E9vhyOgEc/jexLNQybKUjZGww2ek5I2LE3' })3
+      } else if (
+        [LIVE_LIST_API_URL, LIVE_LIST_PAGE_URL].includes(details.url)
+      ) {
+        setHeader(details.requestHeaders, {
+          name: 'Referer',
+          value: LIVE_LIST_PAGE_URL,
+        })
+      }
+    }
 
-    console.log('header', details.requestHeaders);
-    setHeader(details.requestHeaders, { name: 'Referer', value: 'https://liveplatform.taobao.com/live/liveList.htm' })
-    setHeader(details.requestHeaders, { name: 'Origin', value: 'https://databot.taobao.com' })
-    setHeader(details.requestHeaders, { name: 'origin', value: 'https://databot.taobao.com' })
-    setHeader(details.requestHeaders, { name: 'Sec-Fetch-Site', value: 'same-site' })
-    setHeader(details.requestHeaders, { name: 'accept', value: 'application/json' })
-    setHeader(details.requestHeaders, { name: 'bx-ua', value: '125#coPcaW1PcWQTTwQKzNfjQPdSuDf2r14YEXzdh0wkDabeTy+7lLrzizUry2DYvGZJP5aGwjm+HikyYKtwz6J8+ArgPTl5We9NsJtJOiNbljnNvcYsdbW3FLgx2z4tReU3AphNThGsBlOW8t41Mg93Gtr2x5CZy0NQ4acGcUiccgapEgscUXoScvsONhMpc37fJmwP+lKBN+XWapS1SaILV5jdQMmfDZ4AoH7Cllu0VhVulFCP700YSoDhaeU9U89QNLpLDIlnpiMTFtkcSUSOlRqdMcTlw/U/5zenxL8huRUd46mrV0B+oYU66G5mryG5SD4ReFEyPxG9k/HeVABMcKUceFW94TFC+QoZt+4g2e4f2Waf9dV8TH1IEbNOdHPhmFjwPq2MWp0L2ANuaQsZob7Cta31lVeUL34XDx5KFcfml8gt2tL4eIGLJLvb2oW/PikngUF8O/SqLy8Lx7nBI+/lLHIREvY92wh8HGWGhzD/qUMrRbo/poN0zUukj+KixfG+Xo2UV6gOXv5m9tWxeh8g9utz2kOjnZ7D3xpuUrWKrRgOKw1IEzuN095JGkcSRKmAbJh/iT4r4vkXGA/7g66ZLseQz/KphGAr3CQtFBGGVUrx5JiCasvK6XmLGfCOkMl1hdg2UHbY/FSKZ9+PWwpD32qgx5VvrrcWW5CszfQMVdcxFiETZ0E7fa5zs7swj8of7dnKj37GHoRiNRX8w3yTisjU5JphMr2xTzoRY66YCXfGHaAdlev3L11zLoeERm2slIAko8o8936X9ZZPzw3v+0OZzzxaoze7uGpJIcQQCfZXsbdvZwTekRd6VX7X1OVZ/dVGn1E9vhyOgEc/jexLNQybKUjZGww2ek5I2LE3' })
-  
     return { requestHeaders: details.requestHeaders }
   },
   // filters
@@ -51,7 +107,9 @@ chrome.runtime.onInstalled.addListener(function () {
 
   chrome.cookies.getAll({}, function (cookies) {
     const taobaoCookies = cookies.filter((c) => c.domain.includes('taobao.com'))
-    taobaoCookiesStr = taobaoCookies.map((c) => `${c.name}=${c.value}`).join('; ')
+    taobaoCookiesStr = taobaoCookies
+      .map((c) => `${c.name}=${c.value}`)
+      .join('; ')
     console.log({ cookies, taobaoCookies, taobaoCookiesStr })
   })
 
@@ -70,50 +128,32 @@ chrome.runtime.onInstalled.addListener(function () {
 })
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension')
+  console.log(
+    sender.tab
+      ? 'from a content script:' + sender.tab.url
+      : 'from the extension'
+  )
 
   const { type, data } = request
 
   if (type === 'crawlTaobao') {
     console.log({ taobaoCookiesStr })
     ;(async () => {
-      const url = getTaobaoUrl(taobaoCookiesStr)
-      let _data = await fetch(url, {
-        referrerPolicy: 'no-referrer-when-downgrade',
-        body: null,
-        method: 'GET',
-        mode: 'cors',
-        credentials: 'include',
-      })
-      _data = await _data.json()
+      await queryUrl(LIVE_LIST_PAGE_URL)
+      await queryUrl(LIVE_LIST_API_URL)
+      await queryUrl(LIVE_PAGE_URL)
+      const urls = getTaobaoUrlList(taobaoCookiesStr)
+      console.log('==getTaobaoUrlList==', urls)
 
-      console.log('fetch finish', _data)
-
-      sendResponse({ farewell: _data })
+      try {
+        const totalData = await Promise.all(urls.map((url) => fetchTaobao(url)))
+        console.log('fetch finish', totalData)
+        sendResponse({ farewell: totalData })
+      } catch (error) {
+        sendResponse({ farewell: 'error' })
+      }
     })()
 
-    return true
-  }
-
-  if (request.greeting == 'hello') {
-    console.log('start fetch')
-
-    // const cookie = new CookieCache()
-    // console.log({cookie});
-    ;(async () => {
-      //   let _data = await fetch(url, {
-      //     referrerPolicy: 'no-referrer-when-downgrade',
-      //     body: null,
-      //     method: 'GET',
-      //     mode: 'cors',
-      //     credentials: 'include',
-      //   })
-      //   _data = await _data.json()
-
-      console.log('fetch finish', _data)
-
-      sendResponse({ farewell: _data })
-    })()
     return true
   }
 })
--- js/sign.js
+++ js/sign.js
@@ -1,34 +1,34 @@
-function getTaobaoUrl(cookie) {
-  const data = {
-    param: JSON.stringify({
-      queryId: '11|57240225|undefined',
-      cubeId: 'tblive_rpt_item_indicator',
-      queryDetail: false,
-      startTime: '2020-07-07 20:01:09',
-      endTime: '2020-07-07 22:02:59',
-      timeType: 2,
-      sign: null,
-      limit: 2000,
-      row: JSON.stringify([
-        { name: '商品id', isMeasure: false },
-        { name: '主图地址', isMeasure: false },
-        { name: '商品标题', isMeasure: false },
-      ]),
-      measure: JSON.stringify([
-        { name: '商品点击次数', isMeasure: true },
-        { name: '商品点击人数', isMeasure: true },
-      ]),
-      column: JSON.stringify([]),
-      orders: JSON.stringify([]),
-      filter: JSON.stringify([
-        { name: '开播日期分区字段yyyymmdd', type: 'dimension', isMeasure: false, values: ['20200707'], oper: '=' },
-        { name: '直播间id', type: 'dimension', isMeasure: false, values: ['271421666877'], oper: '=' },
-        { name: '主播id', type: 'dimension', isMeasure: false, values: [2081592196], oper: '=' },
-      ]),
-      extra: null,
-    }),
-    innerId: '',
-  }
+function getTaobaoUrl(url, data, cookie) {
+  // const data = {
+  //   param: JSON.stringify({
+  //     queryId: '11|57240225|undefined',
+  //     cubeId: 'tblive_rpt_item_indicator',
+  //     queryDetail: false,
+  //     startTime: '2020-07-07 20:01:09',
+  //     endTime: '2020-07-07 22:02:59',
+  //     timeType: 2,
+  //     sign: null,
+  //     limit: 2000,
+  //     row: JSON.stringify([
+  //       { name: '商品id', isMeasure: false },
+  //       { name: '主图地址', isMeasure: false },
+  //       { name: '商品标题', isMeasure: false },
+  //     ]),
+  //     measure: JSON.stringify([
+  //       { name: '商品点击次数', isMeasure: true },
+  //       { name: '商品点击人数', isMeasure: true },
+  //     ]),
+  //     column: JSON.stringify([]),
+  //     orders: JSON.stringify([]),
+  //     filter: JSON.stringify([
+  //       { name: '开播日期分区字段yyyymmdd', type: 'dimension', isMeasure: false, values: ['20200707'], oper: '=' },
+  //       { name: '直播间id', type: 'dimension', isMeasure: false, values: ['271421666877'], oper: '=' },
+  //       { name: '主播id', type: 'dimension', isMeasure: false, values: [2081592196], oper: '=' },
+  //     ]),
+  //     extra: null,
+  //   }),
+  //   innerId: '',
+  // }
 
   const jsv = '2.5.8'
   const appKey = '27522521'
@@ -41,8 +41,8 @@ function getTaobaoUrl(cookie) {
   const H5Request = 'true'
   const token = getToken(cookie)
 
-  const signParams = `${token}&${t}&${appKey}&${JSON.stringify(data)}`
-  const url = new URL('https://h5api.m.taobao.com/h5/mtop.alibaba.iic.xinsightshop.olap.query/1.0/')
+  const signParams = `${token}&${t}&${appKey}&${data}`
+  url = new URL(url)
   const sign = md5(signParams)
 
   const params = {
@@ -57,10 +57,26 @@ function getTaobaoUrl(cookie) {
     timeout,
     H5Request,
     token,
-    data: JSON.stringify(data),
+    // data: JSON.stringify(data),
+    data
   }
 
-  Object.keys(params).forEach((key) => url.searchParams.append(key, params[key]))
+  Object.keys(params).forEach((key) =>
+    url.searchParams.append(key, params[key])
+  )
 
   return url
+}
+
+function getTaobaoUrlList(cookie) {
+  // const urls = ['https://h5api.m.taobao.com/h5/mtop.alibaba.iic.xinsightshop.olap.query/1.0/']
+  return urls.map((url) => {
+    const { origin, pathname, searchParams } = new URL(url)
+    return getTaobaoUrl(
+      `${origin}${pathname}`,
+      searchParams.get('data'),
+      cookie
+    )
+  })
+  // return urls.map((url) => getTaobaoUrl(url, cookie))
 }
